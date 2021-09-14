4076814b51b86f3446aadd20a54bf03b93689b74
shivangipatodiya <shivu2610@gmail.com>
2021-09-14 02:58:25 +0000

    linted assertion function

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 const assertEqual = function(actual, expected) {
-if(actual === expected){
-  return console.log('✅✅✅Assertion Passed: ' + actual + ' === ' + expected)
-} else{
-  return console.log('🛑🛑🛑Assertion Failed: ' + actual + ' !== ' + expected)
-}
+  if (actual === expected) {
+    return console.log('✅✅✅Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    return console.log('🛑🛑🛑Assertion Failed: ' + actual + ' !== ' + expected);
+  }
 };
 
 // TEST CODE

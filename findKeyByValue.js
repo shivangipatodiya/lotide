1d03c21a5abe5ecf95c361619aa3e35d541d2a17
shivangipatodiya <shivu2610@gmail.com>
2021-09-16 03:11:25 +0000

    findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const findKeyByValue = (object, value) => {
+  for (let key in object) {
+    if (object[key] === value) {
+      return key;
+    }
+  }
+};
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);

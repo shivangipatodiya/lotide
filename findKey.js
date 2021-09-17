060c7eeab813aafdc991ba4107b504e6991828a1
shivangipatodiya <shivu2610@gmail.com>
2021-09-17 00:27:09 +0000

    findKey function

---
+++ findKey.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object, cb) => {
+  for (let key in object) {
+    if (cb(object[key])) {
+      return key;
+    }
+  } return undefined;
+};
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma"); // => "noma"
+
+assertEqual(findKey({a: 1, b: 2, c: -1}, x => x < 0), 'a');

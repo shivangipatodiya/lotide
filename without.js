cbf725ae0a6f6da9bbbb5e549439266af73809a3
shivangipatodiya <shivu2610@gmail.com>
2021-09-20 20:45:13 +0000

    export and require used for head and assert equal

--- without.js
+++ without.js
@@ -1,9 +1,11 @@
 const eqArrays = (list1, list2) => {
-  if (list1.length === list2.length) {
-    return list1.every((value, index) => value === list2[index]);
-  } else {
-    return false;
-  }
+  if(Array.isArray(list1) && Array.isArray(list2)) {
+ if (list1.length === list2.length) {
+   return list1.every((value, index) => value === list2[index]);
+ }
+ } else {
+   return false;
+ }
 };
 const assertArraysEqual = (actual, expected) => {
   if (eqArrays(actual, expected)) {
